0a2059b1b7381f50a1ab6f118014495e91d59fca
Reem99-sina <reemsina2@gmail.com>
2023-09-22 20:46:16 +0300

    fryer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Header from './components/Header'
-import SubBody from './components/SubBody'
+import Header from './Header'
+import SubBody from './SubBody'
 
 function Home() {
   return (
